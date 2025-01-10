c1cc6000c7eed27354932eb215558f372ceeb4dd
raghav3615 <dadhichraghav896@gmail.com>
2025-01-11 02:33:58 +0530

    updated page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import FeaturesSection from './components/FeaturesSection'
 import HowItWorksSection from './components/HowItWorksSection'
 import TestimonialsSection from './components/TestimonialsSection'
 import FAQsSection from './components/FAQsSection'
-import Navbar from '/src/components/Navbar.tsx'
-import Footer from '/src/components/Footer.tsx'
+import Navbar from "@/components/Navbar"
+import Footer from "@/components/Footer"
 
 export default function Home() {
   return (
